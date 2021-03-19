65d4c9058bdd0c803423bfd5fa9b381d9aadc361
Haroon Rafique <haroon.rafique@protonmail.com>
2021-03-19 15:49:28 +0000

    SLURM submission script maker updated for SCARF

--- 00_Initial_Test/1_H_07/Make_SLURM_plotting_script.py
+++ 00_Initial_Test/1_H_07/Make_SLURM_plotting_script.py
@@ -1,31 +1,25 @@
 #!/usr/bin/env python
-# Python script to create a SLURM submission script for PyORBIT
-# 21 March 2019 Haroon Rafique CERN BE-ABP-HSI 
-# 19 March 2021 Haroon Rafique STFC ISIS
+# Python script to create a SLURM plotting script for PyORBIT
+# 11 August 2020 Haroon Rafique CERN BE-ABP-HSI 
 
 import os
   
 #-----------------------------------------------------------------------
 #	SETTINGS
 #-----------------------------------------------------------------------  
-script_name = "SLURM_submission_script.sh"
+script_name = "SLURM_plotting_script.sh"
 
 # Switches
-hyperthreading = True   # Enable hyperthreading
-exclusive = True        # Exclusive nodes (see SLURM documentation)
-autotime = True         # 7 days True, 2 days False
-autotask = False         # Automatically set nodes to maximum tasks
-clean_all = True        # Clean simulation folder before running (False when resuming pickle checkpoint)
-constrain = False       # Force SCARF hardware
-mem_limits = False      # Specify memory limits, default 4GB
+hyperthreading = False	        # Enable hyperthreading
+exclusive = True		# Exclusive (see SLURM documentation)
+autotime = True			# 2 days for short queues, 2 weeks for long queues
+autotask = True			# Automatically set nodes to maximum tasks
 
 # Must be chosen
 
-# ~ queue = 'scarf' # Default 16/20/24 cores/Node
-queue = 'ibis' # isis exclusive 10x SCARF16 nodes 20 cores/Node
-
-# ~ n_nodes = 4 
-n_tasks_tot = 80
+# ~ queue = 'inf-long', 'inf-short', 'batch-long', 'batch-short'
+queue = 'inf-short'
+n_nodes = 1 
 
 space_charge_flag = int(os.getcwd().split('/')[-1][0])
 print 'simulation_parameters: space charge = ', space_charge_flag
@@ -33,57 +27,49 @@ transverse_plane = os.getcwd().split('/')[-1][2]
 print 'simulation_parameters: transverse_plane = ', transverse_plane
 scan_tune = os.getcwd().split('/')[-1][-2:]
 
-jobname = str(space_charge_flag) + '_' + str(transverse_plane) + '_' + str(scan_tune)
+jobname = '6P_' + str(transverse_plane) + '_' + str(scan_tune)
 
 path_to_simulation = os.path.dirname(os.path.realpath(__file__)) # This directory
 
-
 # Optional - have to use with correct switches
-manual_time = '504:00:00'       # manually set using format 'hours:minutes:seconds'
-manual_tasks = 40               # manually change ntasks
-manual_mem_limit = '4000'       # Manually set memory limit
-manual_constraints = '"[scarf17|scarf18]"'     # select multiple SCARF clusters
-manual_constraints = 'scarf17'                 # select single SCARF cluster
+manual_time = '504:00:00' # manually set using format 'hours:minutes:seconds'
+manual_tasks = 40	# manually change ntasks
 
 # Defaults - can be changed
 output_file_name = 'slurm.%N.%j.out'
 error_file_name = 'slurm.%N.%j.err'
-root_dir = '/home/vol05/scarf1015'
+root_dir = '/hpcscratch/user/harafiqu'
 simulation_file = 'pyOrbit.py'
 #-----------------------------------------------------------------------
 #	AUTOMATICALLY FORMAT SCRIPT
 #-----------------------------------------------------------------------  
-n_tasks = 0
+
+n_tasks = 0	
 if autotask:
-        if 'scarf' in queue: 
-                n_tasks = 16 # 20, 24 ??
-        elif 'ibis' in queue: 
-                n_tasks = 20
-        else: 
-                print 'queue not recognised'
-                exit(0)
+	if hyperthreading:
+		if 'batch' in queue: n_tasks = 32
+		elif 'inf' in queue: n_tasks = 40
+		else: 
+			print 'queue not recognised'
+			exit(0)
+
+	else:
+		if 'batch' in queue: n_tasks = 16
+		elif 'inf' in queue: n_tasks = 20
+		else: 
+			print 'queue not recognised'
+			exit(0)
 else: n_tasks = manual_tasks
 
 time = '48:00:00'
 if autotime:
-        time = '168:00:00'
+	if queue == 'batch-short': time = '48:00:00'
+	elif queue == 'inf-short': time = '120:00:00'
+	elif queue == 'inf-long' or 'batch-long': time = '504:00:00'
+	else: 
+		print 'queue not recognised'
+		exit(0)
 else: time = manual_time
-
-constraints = ''
-if constrain:
-        constraints = manual_constraints
-else:
-        if queue == 'scarf': constraints = ''
-        elif queue == 'ibis': constraints = 'scarf16'
-
-mem_limit = '4000'
-if mem_limits:
-        if 'scarf15' or 'scarf16' in constraints: mem_limit = '6000'
-        elif 'scarf17' in constraints: mem_limit = '5000'
-        elif 'scarf18' or 'scarf19' in constraints: mem_limit = '7994' 
-        elif 'gpu13' in constraints: mem_limit = '4000' 
-        elif 'scarf14' or 'gpu14' or 'gpu15' or 'gpu17' or 'scarf20' in constraints: mem_limit = '8000' 
-else: mem_limit = manual_mem_limit
 
 #-----------------------------------------------------------------------
 #	WRITE FILE
@@ -100,14 +86,12 @@ f.write('#!/bin/bash')
 f.write('\n#SBATCH --job-name=' + str(jobname))
 f.write('\n#SBATCH --output=' + str(output_file_name))
 f.write('\n#SBATCH --error=' + str(error_file_name))
-if constrain: f.write('\n#SBATCH --constraint=' + str(constraints))
-f.write('\n#SBATCH --ntasks=' + str(n_tasks_tot))
-# ~ f.write('\n#SBATCH --nodes=' + str(n_nodes))
-# ~ f.write('\n#SBATCH --ntasks-per-node=' + str(n_tasks))
+f.write('\n#SBATCH --nodes=' + str(n_nodes))
+f.write('\n#SBATCH --ntasks-per-node=' + str(n_tasks))
 f.write('\n#SBATCH --partition=' + str(queue))
 f.write('\n#SBATCH --time=' + str(time))
-if mem_limits:  f.write('\n#SBATCH --mem-per-cpu='+str(mem_limit)+'M')
-if exclusive: f.write('\n#SBATCH --exclusive')
+f.write('\n#SBATCH --mem-per-cpu=3200M')
+if (exclusive): f.write('\n#SBATCH --exclusive')
 if not hyperthreading: f.write('\n#SBATCH --hint=nomultithread')
 f.write('\n')
 f.write('\nBATCH_ROOT_DIR=' + str(root_dir))
@@ -116,8 +100,6 @@ f.write('\nOrigIwd=$(pwd)')
 f.write('\n')
 f.write('\n# Make an output folder in the root directory to hold SLURM info file')
 f.write('\ncd ${BATCH_ROOT_DIR}')
-f.write('\noutput_dir="Simulation_SLURM_Outputs"')
-f.write('\nmkdir -p $output_dir')
 f.write('\n')
 f.write('\n# Fill the SLURM info file')
 f.write('\nsimulation_info_file="${BATCH_ROOT_DIR}/${output_dir}/simulation_info_${SLURM_JOB_ID}.${SLURM_NODEID}.${SLURM_PROCID}.txt"')
@@ -135,19 +117,16 @@ f.write('\necho "****************************************" >> ${simulation_info_
 f.write('\n')
 f.write('\n# Enter job directory, clean it, and setup environment -> SLURM info file')
 f.write('\ncd ${RUN_DIR}')
-if clean_all:f.write('\n./clean_all.sh')
 f.write('\n. setup_environment.sh >> ${simulation_info_file}')
-# ~ f.write('\n')
-# ~ f.write('\n# Load correct MPI')
-# ~ f.write('\nmodule load mpi/mpich-3.0-x86_64')
-# ~ f.write('\nmodule load mpi/mpich-x86_64')
-# ~ f.write('\nmodule load mpi/openmpi-x86_64')
+f.write('\n')
+f.write('\n# Load correct MPI')
+f.write('\nmodule load mpi/mvapich2/2.3')
 f.write('\n')
 f.write('\ntstart=$(date +%s)')
 f.write('\n')
 f.write('\n# Run the job')
-if hyperthreading:f.write('\nsrun ${ORBIT_ROOT}/bin/pyORBIT ${RUN_DIR}/' + str(simulation_file))	
-else:f.write('\nsrun --hint=nomultithread ${ORBIT_ROOT}/bin/pyORBIT ${RUN_DIR}/' + str(simulation_file))
+f.write('\nsrun --exclusive -n 1 ./Plot_Tune_and_Distn_Footprints.py &')
+f.write('\nwait')
 f.write('\n')
 f.write('\ntend=$(date +%s)')
 f.write('\ndt=$(($tend - $tstart))')
@@ -155,4 +134,4 @@ f.write('\necho "total simulation time (s): " $dt >> ${simulation_info_file}')
 
 f.close()
 
-print 'SLURM submission script creation finished'
+print 'SLURM plotting script creation finished'
