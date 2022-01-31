9a6a8a084dffa456b0f6ccad245e7271a05c4d1f
Haroon Rafique <haroon.rafique@stfc.ac.uk>
2022-01-31 12:06:56 +0000

    <update>[00] setup and SLURM submission script updated but not tested

---
+++ 00_Initial_Test/1_H_07/SLURM_submission_script.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#SBATCH --job-name=1_H_07
+#SBATCH --output=slurm.%N.%j.out
+#SBATCH --error=slurm.%N.%j.err
+#SBATCH --ntasks=80
+#SBATCH --partition=ibis
+#SBATCH --time=168:00:00
+#SBATCH --constraint="intel"
+#SBATCH --exclusive
+
+ml ifort/2018.3.222-GCC-7.3.0-2.30
+
+BATCH_ROOT_DIR=/home/vol05/scarf1015
+RUN_DIR=/work3/isis/scarf1015/SCARF_PTC_PyORBIT_Test/00_Initial_Test/1_H_07
+OrigIwd=$(pwd)
+
+# Make an output folder in the root directory to hold SLURM info file
+cd ${BATCH_ROOT_DIR}
+output_dir="Simulation_SLURM_Outputs"
+mkdir -p $output_dir
+
+# Fill the SLURM info file
+simulation_info_file="${BATCH_ROOT_DIR}/${output_dir}/simulation_info_${SLURM_JOB_ID}.${SLURM_NODEID}.${SLURM_PROCID}.txt"
+echo "PyOrbit path:  `readlink -f ${ORBIT_ROOT}`" >> ${simulation_info_file}
+echo "Run path:  `readlink -f ${RUN_DIR}`" >> ${simulation_info_file}
+echo "Submit host:  `readlink -f ${SLURM_SUBMIT_HOST}`" >> ${simulation_info_file}
+echo "SLURM Job name:  `readlink -f ${SLURM_JOB_NAME}`" >> ${simulation_info_file}
+echo "SLURM Job ID:  `readlink -f ${SLURM_JOB_ID}`" >> ${simulation_info_file}
+echo "SLURM Nodes allocated:  `readlink -f ${SLURM_JOB_NUM_NODES}`" >> ${simulation_info_file}
+echo "SLURM CPUS per Node:  `readlink -f ${SLURM_CPUS_ON_NODE}`" >> ${simulation_info_file}
+echo "SLURM Node ID:  `readlink -f ${SLURM_NODEID}`" >> ${simulation_info_file}
+echo "SLURM total cores for job:  `readlink -f ${SLURM_NTASKS}`" >> ${simulation_info_file}
+echo "SLURM process ID:  `readlink -f ${SLURM_PROCID}`" >> ${simulation_info_file}
+echo "****************************************" >> ${simulation_info_file}
+
+# Enter job directory, clean it, and setup environment -> SLURM info file
+cd ${RUN_DIR}
+./clean_all.sh
+. setup_environment.sh >> ${simulation_info_file}
+
+tstart=$(date +%s)
+
+# Run the job
+mpirun ${ORBIT_ROOT}/bin/pyORBIT ${RUN_DIR}/pyOrbit.py
+
+tend=$(date +%s)
+dt=$(($tend - $tstart))
+echo "total simulation time (s): " $dt >> ${simulation_info_file}
--- 00_Initial_Test/1_H_07/setup_environment.sh
+++ 00_Initial_Test/1_H_07/setup_environment.sh
@@ -1,23 +1,26 @@
 # script to setup the PyOrbit environment 
 # execute like: . setup_environment.sh
-pyOrbit_dir=/apps/contrib/ptc-pyorbit/PTC-PyORBIT/PyOrbit_CERN/py-orbit
+pyOrbit_dir=/apps/contrib/ptc-pyorbit/PTC-PyORBIT/CERN_PTC-PyORBIT/py-orbit
 
 # HR 18.03.21
 # Note that the custom environment is python 2.7
 # python 3.0 commands will not work inside a PyORBIT script but can be
 # used for post processing
 
-source ${pyOrbit_dir}/customEnvironment.sh
-echo "customEnvironment done"
 source ${pyOrbit_dir}/../virtualenvs/py2.7/bin/activate
 echo "python packages charged"
+
+source ${pyOrbit_dir}/customEnvironment.sh
+echo "customEnvironment done"
+
 which python
 
 # The intel fortran licences are required for PTC. If you are running
 # PyORBIT locally you will likely have to VPN into the STFC network
 # to access the license server
-source ${pyOrbit_dir}/../setup_ifort.sh
-echo "ifort charged (necessary for running)"
+#source ${pyOrbit_dir}/../setup_ifort.sh
+#ml ifort/2018.3.222-GCC-7.3.0-2.30
+#echo "ifort charged (necessary for running)"
 
 ORBIT_ROOT_fullpath=`readlink -f ${ORBIT_ROOT}` 
 echo 
